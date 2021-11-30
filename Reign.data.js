33b4f3a2600f3c27104d3d83f88333ba3affdc43
JHKuhn <joshkuhn02@hotmail.com>
2021-11-30 13:09:25 -0600

    Third Commit

--- Reign.data.js
+++ Reign.data.js
@@ -173,6 +173,6 @@ Module['FS_createPath']('/Reign/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 83, "filename": "/UE4CommandLine.txt"}, {"start": 83, "audio": 0, "end": 36755340, "filename": "/Reign/Content/Paks/Reign-HTML5.pak"}], "remote_package_size": 36755340, "package_uuid": "6388e40e-5fbb-49ad-a25f-f4256c295222"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 83, "filename": "/UE4CommandLine.txt"}, {"start": 83, "audio": 0, "end": 36752894, "filename": "/Reign/Content/Paks/Reign-HTML5.pak"}], "remote_package_size": 36752894, "package_uuid": "c345716a-4ddd-4c8f-b4c6-dc25bd42ae32"});
 
 })();
